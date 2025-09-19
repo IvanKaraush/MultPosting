33e2e7d67f58ed1d743b98529a6547f28647a983
Daria Ovcharuk <daria.shinda@gmail.com>
2025-09-19 16:53:35 +0300

    Added migrations

--- src/MultiPosting/MultiPosting.Api/Controllers/MultiPostController.cs
+++ src/MultiPosting/MultiPosting.Api/Controllers/MultiPostController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Haley.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using MultiPosting.Application.Dto;
 using MultiPosting.Application.Interfaces;
+using Shared.Application.Options;
+using Shared.Domain.Enums;
 
 namespace MultiPosting.Api.Controllers;
 
@@ -9,10 +13,12 @@ namespace MultiPosting.Api.Controllers;
 public class MultiPostController : ControllerBase
 {
     private readonly ISocialMediaFactory _socialMediaFactory;
+    private readonly GoogleOptions _googleOptions;
 
-    public MultiPostController(ISocialMediaFactory socialMediaFactory)
+    public MultiPostController(ISocialMediaFactory socialMediaFactory, IOptions<GoogleOptions> googleOptions)
     {
         _socialMediaFactory = socialMediaFactory;
+        _googleOptions = googleOptions.Value;
     }
 
     [HttpGet]
@@ -21,5 +27,21 @@ public class MultiPostController : ControllerBase
         var socialMediaService = _socialMediaFactory.GetSocialMediaService(request.SocialMedia);
         var userResources = await socialMediaService.GetUserResourceAsync(request.Login);
         return Ok(userResources);
+    }
+    
+    [HttpGet("you-tube")]
+    public IActionResult GetYouTubeChannelUrl()
+    {
+        var paramlist = new List<QueryParam>
+        {
+            new("client_id", _googleOptions.ClientId),
+            new("redirect_uri", _googleOptions.RedirectUri),
+            new("response_type", "code"),
+            new("scope", "email profile https://www.googleapis.com/auth/youtube"),
+            new("access_type", "offline"),
+            new("prompt", "consent"),
+            new("state", nameof(SocialMedia.YouTube))
+        };
+        return Ok(_googleOptions.AuthorizationUrl + "?" + new QueryParamList(paramlist).GetConcatenatedString());
     }
 }
---
+++ src/MultiPosting/MultiPosting.Infrastructure/Migrations/20250918142739_Initial.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiPosting.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:social_media", "none,you_tube,instagram,tik_tok,vk,google");
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserResource",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ImageUrl = table.Column<string>(type: "text", nullable: false),
+                    IsSelected = table.Column<bool>(type: "boolean", nullable: false),
+                    SocialMedia = table.Column<int>(type: "social_media", nullable: false),
+                    ProjectId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserResource", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserResource_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserResource_ProjectId",
+                table: "UserResource",
+                column: "ProjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserResource");
+
+            migrationBuilder.DropTable(
+                name: "Project");
+        }
+    }
+}
